f79229ed5a2d9d86104a299795e685b292cbbd1f
chohan3036 <55574154+chohan3036@users.noreply.github.com>
2019-11-12 00:45:57 +0900

    added codes;sending img to server

--- closetAnd/app/src/main/java/com/example/closet/Main2Activity.java
+++ closetAnd/app/src/main/java/com/example/closet/Main2Activity.java
@@ -1,74 +1,223 @@
 package com.example.closet;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.media.Image;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class Main2Activity extends AppCompatActivity {
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+    String[] REQUESTED_PEERMISSIONS = {Manifest.permission.CAMERA,
+                                       Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+    private static final Pattern IP_ADDRESS
+            = Pattern.compile(
+            "((25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\\.(25[0-5]|2[0-4]"
+                    + "[0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1]"
+                    + "[0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}"
+                    + "|[1-9][0-9]|[0-9]))");
+
+    //final int SELECT_MULTIPLE_IMAGES = 1;
+    ArrayList<String> selectedImagesPaths; // Paths of the image(s) selected by the user.
+    boolean imagesSelected = false; // Whether the user selected at least an image or not.
+
     private static final int PICK_FROM_CAMERA = 0;
     private static final int PICK_FROM_ALBUM = 1;
-    private Uri mImageCaptureUri;
+    private Uri uri;
+
     ImageView imageView;
     Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getApplicationContext(), "이 앱을 실행하려면 인터넷과 외부 저장소 접근 권한이 필요합니다.", Toast.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions(Main2Activity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            ActivityCompat.requestPermissions(Main2Activity.this, new String[]{Manifest.permission.INTERNET}, 2);
+        }
         setContentView(R.layout.activity_main2);
-        imageView = (ImageView)findViewById(R.id.imageTest);
-        doTakeAlbumAction();
-
+        imageView = findViewById(R.id.imageTest);
     }
-    private void doTakeAlbumAction()
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    Toast.makeText(getApplicationContext(), "Access to Storage Permission Granted. Thanks.", Toast.LENGTH_SHORT).show();
+                } else {
+//                    Toast.makeText(getApplicationContext(), "Access to Storage Permission Denied.", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+            case 2: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    Toast.makeText(getApplicationContext(), "Access to Internet Permission Granted. Thanks.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Access to Internet Permission Denied.", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+
+    public void connectServer(View v) {
+        TextView responseText = findViewById(R.id.responseText);
+        if (imagesSelected == false) { // This means no image is selected and thus nothing to upload.
+            responseText.setText("No Image Selected to Upload. Select Image(s) and Try Again.");
+            return;
+        }
+        System.out.println(selectedImagesPaths);
+        responseText.setText("Sending the Files. Please Wait ...");
+
+        EditText ipv4AddressView = findViewById(R.id.IPAddress);
+        String ipv4Address = ipv4AddressView.getText().toString();
+        EditText portNumberView = findViewById(R.id.portNumber);
+        String portNumber = portNumberView.getText().toString();
+
+        Matcher matcher = IP_ADDRESS.matcher(ipv4Address);
+        if (!matcher.matches()) {
+            responseText.setText("Invalid IPv4 Address. Please Check Your Inputs.");
+            return;
+        }
+
+        String postUrl = "http://" + ipv4Address + ":" + portNumber + "/";
+        MultipartBody.Builder multipartBodyBuilder = new MultipartBody.Builder().setType(MultipartBody.FORM);
+
+        for (int i = 0; i < selectedImagesPaths.size(); i++) {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.RGB_565;
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            try {
+                //Bitmap bitmap = BitmapFactory.decodeFile(selectedImagesPaths.get(i), options);
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+            }catch(Exception e){
+                responseText.setText("Please Make Sure the Selected File is an Image.");
+                return;
+            }
+            byte[] byteArray = stream.toByteArray();
+
+            multipartBodyBuilder.addFormDataPart("image" + i, "Android_Flask_" + i + ".jpg", RequestBody.create(MediaType.parse("image/*jpg"), byteArray));
+        }
+
+        RequestBody postBodyImage = multipartBodyBuilder.build();
+
+        postRequest(postUrl, postBodyImage);
+    }
+
+    void postRequest(String postUrl, RequestBody postBody) {
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(postUrl)
+                .post(postBody)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                // Cancel the post on failure.
+                call.cancel();
+                Log.d("FAIL", e.getMessage());
+
+                // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView responseText = findViewById(R.id.responseText);
+                        responseText.setText("Failed to Connect to Server. Please Try Again.");
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView responseText = findViewById(R.id.responseText);
+                        try {
+                            responseText.setText("Server's Response\n" + response.body().string());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+
+    public void doTakeAlbumAction(View v)
     {
         // 앨범 호출
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        //intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
         intent.setType("image/*");
         startActivityForResult(intent, PICK_FROM_ALBUM);
     }
 
     @Override
-
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+
         if (requestCode == PICK_FROM_ALBUM) {
+
+            String currentImagePath;
+            selectedImagesPaths = new ArrayList<>();
+
             if (data == null) {
                 Log.d("Log_d data", "data is null");
             }
             else {
-                mImageCaptureUri = data.getData();
+                uri = data.getData();
+                currentImagePath = DocumentsContract.getDocumentId(uri);
+                selectedImagesPaths.add(currentImagePath);
+                imagesSelected = true;
                 try {
-
-                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),mImageCaptureUri);
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
                     Log.d("Log_d bitmap객체 : ",bitmap.toString());
                     imageView.setImageBitmap(bitmap);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-            /*Log.d("Log_dsssssss4",mImageCaptureUri.toString());
-                Log.d("Log_dsssssss0",data.toString());
-                Log.d("Log_dsssssss1",data.getExtras().toString());
-                Bundle bundle = data.getExtras();
-                Log.d("log_dssssssss2",bundle.toString());
-
-                Bitmap bitmap = (Bitmap)bundle.get("data");
-                //Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-                Log.d("Log_d bitmap", bitmap.toString());
-                */
             }
-
         }
+        super.onActivityResult(requestCode, resultCode, data);
     }
+
 }
