6cac64e1258778310d04ea1ad7a8524923cf1b37
Inyoung <sand1155@naver.com>
2019-11-17 15:45:41 +0900

    pull try

--- closetAnd/app/src/main/java/com/example/closet/Clothes/Clothes.java
+++ closetAnd/app/src/main/java/com/example/closet/Clothes/Clothes.java
@@ -2,18 +2,26 @@ package com.example.closet.Clothes;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 import com.example.closet.R;
 
 public class Clothes extends AppCompatActivity {
 
     View view;
     ImageView clothesIv = null;
+    private int count;
+    private Bitmap[] thumbnails;
+    private boolean[] thumbnailsselection;
+    private String[] arrPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +31,32 @@ public class Clothes extends AppCompatActivity {
         GridView gridview = (GridView) findViewById(R.id.clothes_grid);
         gridview.setAdapter(new Clothes_GridAdapter(this));
 
-        Button btn1 = (Button) findViewById(R.id.add) ;
+        Button btn1 = (Button) findViewById(R.id.add);
         Button btn2 = (Button) findViewById(R.id.mypick) ;
 
         btn1.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 1);
+                final int len = thumbnailsselection.length;
+                int cnt = 0;
+                String selectImages = "";
+                for (int i =0; i<len; i++)
+                {
+                    if (thumbnailsselection[i]){
+                        cnt++;
+                        selectImages = selectImages + arrPath[i] + "|";
+                    }
+                }
+                if (cnt == 0){
+                    Toast.makeText(getApplicationContext(),
+                            "Please select at least one image",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "You've selected Total " + cnt + " image(s).",
+                            Toast.LENGTH_LONG).show();
+                    Log.d("SelectedImages", selectImages);
+                }
             }
         });
 
--- closetAnd/app/src/main/java/com/example/closet/Clothes/Clothes_GridAdapter.java
+++ closetAnd/app/src/main/java/com/example/closet/Clothes/Clothes_GridAdapter.java
@@ -1,19 +1,32 @@
 package com.example.closet.Clothes;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.closet.R;
 
 class Clothes_GridAdapter extends BaseAdapter {
+    private LayoutInflater mInflater;
+    Context context;
+    private boolean[] thumbnailsselection;
+    private String[] arrPath;
+    private Bitmap[] thumbnails;
 
-    private Context context;
     public int[] imglist = {R.drawable.example_01, R.drawable.example_04, R.drawable.example_07};
 
     public Clothes_GridAdapter(Context c) {
-        context = c;
+        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
@@ -23,28 +36,76 @@ class Clothes_GridAdapter extends BaseAdapter {
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return position;
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
-
+        ViewHolder holder;
         if (convertView == null) {
-            imageView = new ImageView(context);
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(8, 8, 8, 8);
+            holder = new ViewHolder();
+            convertView = mInflater.inflate(R.layout.clothes_griditem, null);
+            holder.imageview = (ImageView) convertView.findViewById(R.id.clothes_iv);
+            holder.checkbox = (CheckBox) convertView.findViewById(R.id.chk_clothes_iv);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        holder.checkbox.setId(position);
+        holder.imageview.setId(position);
+        holder.checkbox.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                CheckBox cb = (CheckBox) v;
+                int id = cb.getId();
+                if (thumbnailsselection[id]) {
+                    cb.setChecked(false);
+                    thumbnailsselection[id] = false;
+                } else {
+                    cb.setChecked(true);
+                    thumbnailsselection[id] = true;
+                }
+            }
+        });
+        holder.imageview.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                int id = v.getId();
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setDataAndType(Uri.parse("file://" + arrPath[id]), "image/*");
+                context.startActivity(intent);
+            }
+        });
+        holder.imageview.setImageBitmap(thumbnails[position]);
+        holder.checkbox.setChecked(thumbnailsselection[position]);
+        holder.id = position;
+        return convertView;
+    }
+
+    class ViewHolder {
+        ImageView imageview;
+        CheckBox checkbox;
+        int id;
+    }
+}
+/* if (convertView == null) {
+        imgBtn = new ImageButton(context);
+        imgBtn.setScaleType(ImageButton.ScaleType.CENTER_CROP);
+        imgBtn.setPadding(8, 8, 8, 8);
         }
         else
         {
-            imageView = (ImageView) convertView;
+        imgBtn = (ImageButton) convertView;
         }
-        imageView.setImageResource(imglist[position]);
-        return imageView;
-    }
-}
+        imgBtn.setImageResource(imglist[position]);
+        return imgBtn;
+*/
