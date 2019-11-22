c6019ef7bff15126e299ad6818657ad666c8b6c6
Inyoung <sand1155@naver.com>
2019-11-22 13:56:26 +0900

    for push

--- closetAnd/app/src/main/java/com/example/closet/History/History.java
+++ closetAnd/app/src/main/java/com/example/closet/History/History.java
@@ -5,45 +5,33 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.GridView;
 import android.widget.RelativeLayout;
+
 import com.example.closet.R;
 
 public class History extends Fragment {
 
     View view;
-
-
     public int[] imageIDs = new int[]{R.drawable.example_01, R.drawable.example_04, R.drawable.example_07};
 
     public History() {
         // Required empty public constructor
     }
-}
-/*
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-    {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view =inflater.inflate(R.layout.fragment_history, container, false);
+        view = inflater.inflate(R.layout.fragment_match, container, false);
+        GridView gridview = (GridView) view.findViewById(R.id.clothes_grid);
+        gridview.setAdapter(new History_GridAdapter(this, imageIDs));
 
         return view;
     }
-
 }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_clothes);
-
-        GridView gridViewImages = (GridView) findViewById(R.id.clothes_grid);
-        GridAdapter imageGridAdapter = new GridAdapter(this, imageIDs);
-        gridViewImages.setAdapter(imageGridAdapter);
-    }
- */
