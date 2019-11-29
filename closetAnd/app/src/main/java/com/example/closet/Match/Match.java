d05f9c6d180f063921f63cdb4dccdcd108dc64d1
KOSUBIN <dmsql2929@naver.com>
2019-11-29 15:48:18 +0900

    add break

--- closetAnd/app/src/main/java/com/example/closet/Match/Match.java
+++ closetAnd/app/src/main/java/com/example/closet/Match/Match.java
@@ -30,7 +30,7 @@ import androidx.fragment.app.Fragment;
 public class Match extends Fragment implements View.OnClickListener {
     private PopupWindow mPopupWindow;
     View view;
-    Button save, mypick, reset;
+    Button save, pick, reset;
     ArrayList<URL> selected_from_clothes = new ArrayList<>();
 
 
@@ -51,10 +51,12 @@ public class Match extends Fragment implements View.OnClickListener {
         super.onCreateView(inflater, container, savedInstanceState);
         view = inflater.inflate(R.layout.fragment_match, container, false);
 
+        pick = (Button) view.findViewById(R.id.pick);
+        pick.setOnClickListener(this);
+
         save = (Button) view.findViewById(R.id.save);
         save.setOnClickListener(this);
-        mypick = (Button) view.findViewById(R.id.mypick);
-        mypick.setOnClickListener(this);
+
         reset = (Button) view.findViewById(R.id.reset);
         reset.setOnClickListener(this);
 
@@ -63,61 +65,55 @@ public class Match extends Fragment implements View.OnClickListener {
 
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.save: {
-                Popup();
-            }
-            case R.id.reset: {
-            }
-            case R.id.mypick: {
-                Move(); // match_grid로 이동
-            }
-        }
-    }
+            case R.id.pick:
+                Intent intent = new Intent(getContext(), Match_Grid.class);
+                //intent.putExtra("selected_items", selected_from_clothes);
+                startActivity(intent); // match_grid로 이동
+                break;
 
-    protected void Popup() {
-        final View popupView = getLayoutInflater().inflate(R.layout.match_pop_up, null);
-        mPopupWindow = new PopupWindow(popupView, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        mPopupWindow.setFocusable(true);
-        mPopupWindow.showAtLocation(popupView, Gravity.CENTER, 0, 0);
+            case R.id.save:
+                final View popupView = getLayoutInflater().inflate(R.layout.match_pop_up, null);
+                mPopupWindow = new PopupWindow(popupView, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                mPopupWindow.setFocusable(true);
+                mPopupWindow.showAtLocation(popupView, Gravity.CENTER, 0, 0);
 
-        Spinner spinner = (Spinner) popupView.findViewById(R.id.match_save_spinner);
-        String[] Look_item = getResources().getStringArray(R.array.match_array);
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, Look_item);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(dataAdapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                // On selecting a spinner item
-                String Look = parent.getItemAtPosition(position).toString();
-                if (!Look.equals("Look")) {
-                    Toast.makeText(parent.getContext(), "Selected: " + Look, Toast.LENGTH_LONG).show();
-                    Button ok = (Button) popupView.findViewById(R.id.match_save_Ok);
-                    ok.setOnClickListener(new View.OnClickListener() {
-                        public void onClick(View v) {
-                            //save networkigng 해야하는 곳(아바타랑 Look 같이 보내주기)
-                            mPopupWindow.dismiss();
+                Spinner spinner = (Spinner) popupView.findViewById(R.id.match_spinner);
+                String[] Look_item = getResources().getStringArray(R.array.match_array);
+                ArrayAdapter<String> dataAdapter3 = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, Look_item);
+                dataAdapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(dataAdapter3);
+                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        // On selecting a spinner item
+                        String Look = parent.getItemAtPosition(position).toString();
+                        if (!Look.equals("Look")) {
+                            Toast.makeText(parent.getContext(), "Selected: " + Look, Toast.LENGTH_LONG).show();
+                            Button ok = (Button) popupView.findViewById(R.id.match_save_Ok);
+                            ok.setOnClickListener(new View.OnClickListener() {
+                                public void onClick(View v) {
+                                    //save networkigng 해야하는 곳(아바타랑 Look 같이 보내주기)
+                                    //mPopupWindow.dismiss();
+                                }
+                            });
                         }
-                    });
-                }
-            }
+                    }
 
-            public void onNothingSelected(AdapterView<?> arg0) {
-                // TODO Auto-generated method stub
-            }
-        });
+                    public void onNothingSelected(AdapterView<?> arg0) {
+                        // TODO Auto-generated method stub
+                    }
+                });
 
-        Button cancel = (Button) popupView.findViewById(R.id.match_save_Cancel);
-        cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mPopupWindow.dismiss();
-            }
-        });
-    }
+                Button cancel = (Button) popupView.findViewById(R.id.match_save_Cancel);
+                cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mPopupWindow.dismiss();
+                    }
+                });
 
-    protected void Move() {  // mypick 버튼 누르면 Match_Grid로 이동
-        Intent intent = new Intent(getContext(), Match_Grid.class);
-        //intent.putExtra("selected_items", selected_from_clothes);
-        startActivity(intent);
+            break;
+            case R.id.reset: break;
+
+        }
     }
 }
