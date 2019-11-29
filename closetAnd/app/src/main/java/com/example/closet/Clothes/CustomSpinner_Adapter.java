577183a046030bef36a671b4d40d6fbd8a880b6b
KOSUBIN <dmsql2929@naver.com>
2019-11-29 17:54:53 +0900

    add spinner image and color

---
+++ closetAnd/app/src/main/java/com/example/closet/Clothes/CustomSpinner_Adapter.java
@@ -0,0 +1,78 @@
+package com.example.closet.Clothes;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.example.closet.R;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.example.closet.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class CustomSpinner_Adapter extends ArrayAdapter<String> {
+
+    String[] spinnerNames;
+    int[] spinnerImages;
+    Context mContext;
+
+    public CustomSpinner_Adapter(Context context, String[] names, int[] images) {
+        super(context, R.layout.spinner_row);
+
+        this.spinnerNames = names;
+        this.spinnerImages = images;
+        this.mContext = context;
+    }
+
+    @Override
+    public View getDropDownView(int position,  View convertView, ViewGroup parent) {
+        return getView(position, convertView, parent);
+    }
+
+    @Override
+    public int getCount() {
+        return spinnerNames.length;
+    }
+
+
+    @Override
+    public View getView(int position,  View convertView,ViewGroup parent) {
+
+        ViewHolder mViewHolder = new ViewHolder();
+
+        if (convertView == null) {
+
+            LayoutInflater mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = mInflater.inflate(R.layout.spinner_row, parent, false);
+
+            mViewHolder.mImage = (ImageView) convertView.findViewById(R.id.imageview_spinner_image);
+            mViewHolder.mName = (TextView) convertView.findViewById(R.id.textview_spinner_name);
+            convertView.setTag(mViewHolder);
+
+        } else {
+
+            mViewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        mViewHolder.mImage.setImageResource(spinnerImages[position]);
+        mViewHolder.mName.setText(spinnerNames[position]);
+
+        return convertView;
+    }
+
+    private static class ViewHolder {
+
+        ImageView mImage;
+        TextView mName;
+    }
+}
