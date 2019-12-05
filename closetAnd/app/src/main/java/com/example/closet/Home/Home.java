53178fbeb4da113039a51ac9af80b79d725b2b5a
Inyoung <sand1155@naver.com>
2019-12-05 21:45:57 +0900

    for push

--- closetAnd/app/src/main/java/com/example/closet/Home/Home.java
+++ closetAnd/app/src/main/java/com/example/closet/Home/Home.java
@@ -1,6 +1,8 @@
 package com.example.closet.Home;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
